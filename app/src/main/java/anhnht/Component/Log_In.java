b2376a6856c0655a22b8297dea4fd9d4217a2d9d
trungnt2317 <nguyentrantrung2311@gmail.com>
2017-10-24 22:50:43 +0700

    login and register

--- app/src/main/java/anhnht/Component/Log_In.java
+++ app/src/main/java/anhnht/Component/Log_In.java
@@ -1,25 +1,35 @@
 package anhnht.Component;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.anhnh.sliderdemo.MainActivity;
 import com.example.anhnh.sliderdemo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Log_In extends AppCompatActivity {
 
     private EditText txtEmail, txtPass;
     private Button btnLogin;
     private TextView txtRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in);
+        mAuth = FirebaseAuth.getInstance();
+
         init();
     }
 
@@ -34,23 +44,30 @@ public class Log_In extends AppCompatActivity {
                 setErrorText(txtPass);
                 return;
             }
-            //Login function
-
-            //If success return to main page with code RESULT_OK
-                //setResult(RESULT_OK);
-                // set bundle to return username
-                //Bundle bundle = new Bundle();
-                // bundle.putString("USERNAME", USERNAME_OF_DB);
-    //            Intent intent = new Intent();
-    //            intent.putExtras(bundle);
-    //            setResult(Activity.RESULT_OK, intent);
-    //            finish();
-            //Else
+            loginUser(email, pass);
                 // txtEmail.setError
         }else if(view == txtRegister){
             Intent regist = new Intent(this, Register.class);
             startActivity(regist);
         }
+    }
+
+    private void loginUser(String email, String pass) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Log_In.this, " log in successful", Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(Log_In.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(mainIntent);
+
+                    finish();
+                }else {
+                    Toast.makeText(Log_In.this, "not successful", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void init(){
--- app/src/main/java/anhnht/Component/Movie_Information_Activity.java
+++ app/src/main/java/anhnht/Component/Movie_Information_Activity.java
@@ -2,16 +2,29 @@ package anhnht.Component;
 
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.anhnh.sliderdemo.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Movie_Information_Activity extends AppCompatActivity {
 
@@ -19,12 +32,52 @@ public class Movie_Information_Activity extends AppCompatActivity {
     private TypedArray image;
     TextView movie_des, movieName;
     Button btnTrailer;
+    private DatabaseReference mUserReference;
+
+    private FirebaseUser currentUser;
+    private StorageReference mStorageRef;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie__information_);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = currentUser.getUid();
+        mUserReference = FirebaseDatabase.getInstance().getReference().child("Films");
+        mUserReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Toast.makeText(Movie_Information_Activity.this, dataSnapshot.toString(), Toast.LENGTH_LONG).show();
+                String name = dataSnapshot.child("Film1").child("Name").getValue().toString();
+                movieName.setText(name);
+
+                String des = dataSnapshot.child("Film1").child("Description").getValue().toString();
+                movie_des.setText(des);
+
+                String url = dataSnapshot.child("Film1").child("Picture").child("image1").getValue().toString();
+                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                imageView.setImageBitmap(bmp);
+
+
+
+                Toast.makeText(Movie_Information_Activity.this, url, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+
+
         final int pos = (int) getIntent().getExtras().get("Position");
         //Set movie poster
         imageView= (ImageView) findViewById(R.id.movie_view);
