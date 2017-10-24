e255decb38337597b81021836e4dd38c10b564e1
Anh Nguyen <anhnht1996@gmail.com>
2017-10-24 23:21:47 +0700

    anhnht

---
+++ app/src/main/java/anhnht/Component/Booking_Ticket.java
@@ -0,0 +1,15 @@
+package anhnht.Component;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anhnh.sliderdemo.R;
+
+public class Booking_Ticket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking__ticket);
+    }
+}
